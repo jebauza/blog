0feb191bc9a2eee5534c82e72881fbb7a46d1a11
Jorge Ernesto Bauza <jebauza1989@gmail.com>
2022-02-05 13:46:11 +0100

    ready tailwindcss for develop

--- public/js/app.js
+++ public/js/app.js
@@ -22726,6 +22726,19 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/**
 
 /***/ }),
 
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/css/app.css":
 /*!*******************************!*\
   !*** ./resources/css/app.css ***!
@@ -23067,7 +23080,8 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/app": 0
+/******/ 			"css/app": 0,
+/******/ 			"css/appSass": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -23117,8 +23131,9 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/appSass"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/appSass"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/appSass"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+//const colors = require('tailwindcss/colors');
 
 module.exports = {
     content: [
@@ -13,8 +14,58 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+
+            // colors: {
+            //     transparent: 'transparent',
+            //     current: 'currentColor',
+            //     //amber: colors.amber,
+            //     black: colors.black,
+            //     blue: colors.blue,
+            //     cyan: colors.cyan,
+            //     emerald: colors.emerald,
+            //     fuchsia: colors.fuchsia,
+            //     gray: colors.gray,
+            //     green: colors.green,
+            //     indigo: colors.indigo,
+            //     lime: colors.lime,
+            //     orange: colors.orange,
+            //     pink: colors.pink,
+            //     purple: colors.purple,
+            //     red: colors.red,
+            //     rose: colors.rose,
+            //     sky: colors.sky,//warn - As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`.
+            //     teal: colors.teal,
+            //     violet: colors.violet,
+            //     yellow: colors.amber,
+            //     white: colors.white,
+            // },
         },
     },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+
+    // Mode jit (Just-in-Time engine) esta presente siempre en tailwind V3, asi se carga todas las clases css
+    // safelist: [
+    //     {
+    //       pattern: /.*/,
+    //       variants: [
+    //         "first",
+    //         "last",
+    //         "odd",
+    //         "even",
+    //         "visited",
+    //         "checked",
+    //         "empty",
+    //         "read-only",
+    //         "group-hover",
+    //         "group-focus",
+    //         "focus-within",
+    //         "hover",
+    //         "focus",
+    //         "focus-visible",
+    //         "active",
+    //         "disabled",
+    //       ],
+    //     },
+    //   ],
 };
